0d302d651f1cb5f937e4f70e05a6578f2c6d3ed8
kdncode <kdncode@gmail.com>
2018-05-02 02:46:35 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express'),
+      morgan = require('morgan'),
+      bodyParser = require('body-parser'),
+      mongoose = require('mongoose'),
+      hbs = require('hbs'),
+      expressHbs = require('express-handlebars');
+
+const app = express();
+
+app.engine('.hbs', expressHbs( { defaultLayout: 'layout', extname: '.hbs'}));
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( { extended : true }));
+
+const mainRoutes = require('./routes/main');
+
+app.use(mainRoutes);
+
+
+app.listen(3030,( err ) => {
+    if (err) console.log(err);
+    console.log(`Server is running on port ${3030}`)
+})
